8ab724e623062ca8b6d3cfbe9326d758c20bd348
Rahul1532 <pandeyrahul1564@email.com>
2023-10-04 10:41:20 +0530

    New File

--- add.cpp
+++ add.cpp
@@ -5,5 +5,21 @@ class add
     private:
     int a;
     int b;
-    int c;
+    
+    public:
+    add(int x,int y)
+    {
+        a=x;
+        b=y;
+    }
+    int sum()
+    {
+        return a+b;
+    }
+};
+int main()
+{
+    add a(5 , 5) ;
+    cout<<a.sum();
+
 }
