e046beb1d6f85917d0d708bb164501ce49e5f1e7
Rahul1532 <pandeyrahul1564@email.com>
2023-10-05 19:24:10 +0530

    New File

---
+++ complex.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+    public:
+    int real;
+    int img;
+    
+    complex(int r=0,int i=0)
+    {
+        real=r;
+        img=i;
+    }
+    complex add(complex s)
+    {
+        complex temp;
+        temp.real= real+s.real;
+        temp.img = img+s.img;
+        return temp;
+
+    }
+};
+int main()
+{
+    complex c1(3,7);
+    complex c2(10,5);
+    complex c3;
+    c3=c1.add(c2);
+    cout<<c3.real<<"+i"<<c3.img<<endl;
+}
