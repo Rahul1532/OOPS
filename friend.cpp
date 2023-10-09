3df4c32fda988e7d5c06c266e42b38423948b2d3
Rahul1532 <pandeyrahul1564@email.com>
2023-10-09 20:50:57 +0530

    New file

---
+++ friend.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    private : int a;
+    public : int b;
+    protected : int c;
+    friend void fun();
+};
+void fun()
+{
+    test t;
+    t.a=10;
+    t.b=20;
+    t.c=30;
+
+}
+int main()
+{
+}
