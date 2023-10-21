377fac5c125a5b7bea0d2d14d7f35ae094d26ddd
Rahul1532 <pandeyrahul1532@gmail.com>
2023-10-21 14:55:48 +0530

    new file

---
+++ arr.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10];
+    int n=10;
+    int l,h,mid;
+    for (int  i = 0; i < n; i++)
+    {
+        cin>>a[i];
+        cout<<a[i]<<" ";
+    }
+}
